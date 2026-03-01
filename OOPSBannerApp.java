a0f0a0f2f0c26ac4a474fa67cbcf200a0060a5eb
sd3458 <sd3458@srmist.edu.in>
2026-03-01 17:09:00 +0530

    UC2: ASCII Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,11 +5,17 @@
  * Oriented Programming System OOPS acronym to the console.
  *
  * @author Satviik Daga
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
-    public static void main(String[] args){
-        System.out.println("OOPS");
+        public static void main(String[] args){
+            System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "  + "   ***** ");
+            System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "  + " **      ");
+            System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "  + "**       ");
+            System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "  + "  *****  ");
+            System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "  + "      ** ");
+            System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "  + " **   ** ");
+            System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "  + "  *****  ");
         }
     }
